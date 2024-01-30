1ec7892e6f5f63164be8aa3a71d0b68f06495a6b
wzseba <wzseba@gmail.com>
2024-01-30 17:19:11 -0300

    add: parseo de datos y ruta por defualt

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+import "dotenv/config";
+import "./database/connectdb.js";
 import express from "express";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("Sever on http://localhost:5000"));
+app.use(express.json());
+
+app.use("/api/v1", authRouter);
+
+app.listen(`${PORT}`, () => console.log(`Sever on http://localhost:${PORT}`));
