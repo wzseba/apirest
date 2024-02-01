78d6bb2f9415662a753a638a11be2d6a04e80f91
wzseba <wzseba@gmail.com>
2024-01-31 22:15:25 -0300

    add: se agrego cookieParser

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import "./database/connectdb.js";
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
