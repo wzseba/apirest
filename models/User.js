6dd5a0782446daab2cd88ba2f7ca85a481d34f79
wzseba <wzseba@gmail.com>
2024-01-31 22:16:25 -0300

    add: se agregaron metodos para el tratamiento de password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose, { model } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const { Schema } = mongoose;
 
@@ -14,5 +15,22 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  // if (!user.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Falló el hash del password");
+  }
+});
+
+userSchema.methods.comparePassword = async function (clientPassword) {
+  return await bcrypt.compare(clientPassword, this.password);
+};
 
 export const User = model("user", userSchema);
