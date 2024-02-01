1fc88fb900f6228dceda35c95b0cb0304c73dc5d
wzseba <wzseba@gmail.com>
2024-01-31 22:14:39 -0300

    add: se agregaron nuevas rutas

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,20 @@
-import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { Router } from "express";
+import {
+  infoUser,
+  login,
+  logout,
+  refreshToken,
+  register,
+} from "../controllers/auth.controller.js";
 import { validatorRegister } from "../middlewares/validations.js";
+import { requireToken } from "../middlewares/requireToken.js";
 
-const router = express.Router();
+const router = Router();
 
 router.post("/register", validatorRegister, register);
 router.post("/login", login);
+router.get("/protected", requireToken, infoUser);
+router.get("/refresh", refreshToken);
+router.get("/logout", logout);
 
 export default router;
