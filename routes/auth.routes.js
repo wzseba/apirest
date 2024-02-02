5087f344535a8a9fa3bf2921a6f22d670c64aa9a
wzseba <wzseba@gmail.com>
2024-02-01 23:26:02 -0300

    add: se agrego requerimiento de refreshToken

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,13 +8,14 @@ import {
 } from "../controllers/auth.controller.js";
 import { validatorRegister } from "../middlewares/validations.js";
 import { requireToken } from "../middlewares/requireToken.js";
+import { requireRefreshToken } from "../middlewares/requireRefreshToken.js";
 
 const router = Router();
 
 router.post("/register", validatorRegister, register);
 router.post("/login", login);
 router.get("/protected", requireToken, infoUser);
-router.get("/refresh", refreshToken);
+router.get("/refresh", requireRefreshToken, refreshToken);
 router.get("/logout", logout);
 
 export default router;
